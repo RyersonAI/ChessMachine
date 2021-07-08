3fd45557ef777493824c31fc7d965df593fa39c5
Zarar Butt <z6butt@ryerson.ca>
2021-07-07 23:54:02 -0400

    Cleaned up repo

--- server.py
+++ server.py
@@ -1,1 +1,21 @@
+from ChessAgent import Agent
+from ChessVision import Vision
+import sockets 
 
+
+
+def main():
+
+
+    agent = Agent()
+    vision = Vision() 
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+
+    main()
